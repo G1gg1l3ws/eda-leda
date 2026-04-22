ba35d5eaea052cbdc16ad50397edf43de58dc876
G1gg1l3ws <guigo.gda@gmail.com>
2026-04-22 15:25:13 -0300

    MoveImpostor returns SyntaxError on tst, despite compiling and executing on local machines

---
+++ src/MoveImpostor.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.io.*;
+
+public class MoveImpostor {
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            int[] array = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            System.out.println(Arrays.toString(moveImpostor(array)));
+        } catch(IOException ioe) {}
+    }
+
+    public static int[] moveImpostor(int[] in) {
+        for (int i = 0; i < in.length - 1; i++) {
+            int x = in[i];
+            int y = in[i + 1];
+            if (x > y) {
+                for (int j = i + 1; j > 0; j--) {
+                    if (in[j] < in[j - 1]) {
+                        in[j] = in[j] ^ in[j - 1];
+                        in[j - 1] = in[j] ^ in[j - 1];
+                        in[j] = in[j] ^ in[j - 1];
+                    }
+                }
+                return in;
+            }
+        }
+        return in;
+    }
+}
---
+++ src/Potencia.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+class Potencia {
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            int p = Integer.parseInt(reader.readLine());
+
+            int e = Integer.parseInt(reader.readLine());
+
+            System.out.println(exponencial(p, e));
+
+        } catch (IOException ioe) {}
+    }
+    public static int exponencial(int p, int e) {
+        if (e == 0) {
+            return 1;
+        }
+        
+        return p * exponencial(p, e-1);
+    }
+}
