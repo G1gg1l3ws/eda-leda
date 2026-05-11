5ba21982a4ae9abd4b8d96c0da9be59e80229271
G1gg1l3ws <guigo.gda@gmail.com>
2026-05-10 23:17:47 -0300

    24/0/75

---
+++ src/BubbleSort.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int[] v = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        
+        sort(v);
+
+    }
+
+    public static void sort(int[] v) {
+        int[] copia = Arrays.copyOf(v, v.length);
+        for (int i = 0; i < v.length; i++) {
+            boolean trocou = false;
+            for (int j = 0; j < v.length-1-i; j++) {
+                if (v[j] > v[j+1]) {
+                    swap(v, j, j+1);
+                    trocou = true;
+                }
+            }
+            if (trocou) {
+                printVetor(v);
+            }
+            else if (Arrays.equals(v, copia)) {
+                printVetor(v);
+                break;
+            } else
+                break;
+        }
+    }
+
+    public static void printVetor(int[] v) {
+        String out = "";
+
+        for (int i = 0; i < v.length; i++) {
+            if (i >= v.length-1) {
+                out += v[i];
+            } else
+                out += v[i] + " ";
+        }
+
+        System.out.println(out);
+    }
+
+    public static void swap(int[] v, int i, int j) {
+        int aux = v[i];
+        v[i] = v[j];
+        v[j] = aux;
+    }
+}
