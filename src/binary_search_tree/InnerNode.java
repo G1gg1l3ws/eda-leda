f1be3f56c23f9ed00c475b99fdf38f0f69d9e180
G1gg1l3ws <guigo.gda@gmail.com>
2026-07-07 07:40:48 -0300

    69/0/30

---
+++ src/binary_search_tree/InnerNode.java
@@ -0,0 +1,280 @@
+import java.util.*;
+
+class InnerNode {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] v = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        BST t = new BST();
+
+        for (int i = 0; i < v.length; i++) {
+            t.add(v[i]);
+        }
+
+        System.out.println(t.contaInternos());
+        sc.close();
+    }
+}
+
+class BST {
+
+    private Node root;
+    private int size;
+    
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+    
+    /**
+     * Implementação iterativa da adição de um elemento em uma árvore binária de pequisa.
+     * @param element o valor a ser adicionado na árvore.
+     */
+    public void add(int element) {
+        this.size += 1;
+        if (isEmpty())
+            this.root = new Node(element);
+        else {
+            
+            Node aux = this.root;
+            
+            while (aux != null) {
+                
+                if (element < aux.value) {
+                    if (aux.left == null) { 
+                        Node newNode = new Node(element);
+                        aux.left = newNode;
+                        newNode.parent = aux;
+                        return;
+                    }
+                    
+                    aux = aux.left;
+                } else {
+                    if (aux.right == null) { 
+                        Node newNode = new Node(element);
+                        aux.right = newNode;
+                        newNode.parent = aux;
+                        return;
+                    }
+                    
+                    aux = aux.right;
+                }
+            }
+        }
+        
+    }
+    
+    
+    /**
+     * Busca o nó cujo valor é igual ao passado como parâmetro. Essa é a implementação 
+     * iterativa clássica da busca binária em uma árvore binária de pesquisa.
+     * @param element O elemento a ser procurado.
+     * @return O nó contendo o elemento procurado. O método retorna null caso
+     * o elemento não esteja presente na árvore.
+     */
+    public Node search(int element) {
+        if (isEmpty()) return null;
+
+        Node current = root;
+
+        while (current != null) {
+            if (current.value == element) return current;
+            else if (element < current.value) current = current.left;
+            else current = current.right;
+        }
+
+        return null;
+    }
+    
+    
+    /**
+     * Retorna a altura da árvore.
+     */
+    public int height() {
+        if (this.isEmpty()) return -1;
+        return this.height(this.root);
+    }
+    private int height(Node current) {
+        if (current == null) return -1;
+        return 1 + Math.max(height(current.left), height(current.right));
+    }
+
+
+    public boolean equals(BST outra) {
+        if (this.isEmpty() && outra.isEmpty()) return true;
+
+        else return this.equals(this.root, outra.root);
+    }
+    private boolean equals(Node current, Node outro) {
+        if (current == null && outro == null) {
+            return true;
+        } else if (current != null && outro != null) {
+            if (current.value != outro.value) {
+                return false;
+            }
+
+            return equals(current.left, outro.left) && equals(current.right, outro.right);
+
+        } else return false;
+    }
+
+    /**
+    * Retorna o número de nós internos da árvore.
+    */
+    public int contaInternos() {
+        if (this.isEmpty()) return 0;
+
+        return contaInternos(this.root);
+    }
+
+    private int contaInternos(Node node) {
+        if (node == null) {
+            return 0;
+        }
+
+        if (!node.isLeaf()) {
+            return 1 + contaInternos(node.left) + contaInternos(node.right);
+        }
+        
+        return contaInternos(node.left) + contaInternos(node.right);
+    }
+
+    /**
+     * @return o tamanho da árvore.
+     */
+    public int size() {
+        return this.size;
+    }
+
+    public String preOrder() {
+        return preOrder(this.root);
+
+    }
+    
+    private String preOrder(Node current) {
+        if (current == null) return "";
+        
+        else {
+            return current.value + " " + preOrder(current.left) + preOrder(current.right);
+        }
+    }
+
+    public String inOrder() {
+        return inOrder(this.root);
+
+    }
+    
+    private String inOrder(Node current) {
+        if (current == null) return "";
+        
+        else {
+            return inOrder(current.left) + " " + current.value + inOrder(current.right);
+        }
+    }
+
+    public String postOrder() {
+        return postOrder(this.root);
+    }
+
+    private String postOrder(Node current) {
+        if (current == null) return "";
+        
+        else {
+            return postOrder(current.left) + postOrder(current.right) + " " +  current.value ;
+        }
+    }
+
+    public void remove(int val) {
+        remove(search(val));
+    }
+
+    private void remove(Node toRemove) {
+        if (this.isEmpty()) throw new RuntimeException();
+
+        if (toRemove == null) {
+            return;
+        }
+
+        if (toRemove.isLeaf()) {
+            if (toRemove == this.root) {
+                this.root = null;
+            } else {
+                if (toRemove.value < toRemove.parent.value) toRemove.parent.left = null;
+                else toRemove.parent.right = null;
+            }
+        } else if (toRemove.hasLeft() && !toRemove.hasRight()) {
+            if (toRemove == this.root) {
+                this.root = toRemove.left;
+                this.root.parent = null;
+            } else {
+                toRemove.left.parent = toRemove.parent;
+                if (toRemove.value < toRemove.parent.value) toRemove.parent.left = toRemove.left;
+                else toRemove.parent.right = toRemove.left;
+            }
+        } else if (!toRemove.hasLeft() && toRemove.hasRight()) {
+            if (toRemove == this.root) {
+                this.root = toRemove.right;
+                this.root.parent = null;
+            } else {
+                toRemove.right.parent = toRemove.parent;
+
+                if (toRemove.value < toRemove.parent.value) toRemove.parent.left = toRemove.right;  
+                else toRemove.parent.right = toRemove.right;
+            }
+        } else {
+            Node sucessor = this.sucessor(toRemove);
+
+            toRemove.value = sucessor.value;
+
+            remove(sucessor);
+        }
+
+        this.size -= 1;
+    }
+
+    public Node sucessor(Node node) {
+        if (node == null) return null;
+
+        if (node.right != null) {
+            return min(node);
+        }
+
+        Node aux = node;
+        while (aux != null && aux.value < node.value) {
+            aux = aux.parent;
+        }
+
+        return aux;
+
+    }
+
+    private Node min(Node current) {
+        if (current.left == null) return current;
+        else return min(current.left);
+    }
+}
+
+class Node {
+    
+    int value;
+    Node left;
+    Node right;
+    Node parent;
+    
+    Node(int v) {
+        this.value = v;
+    }
+
+    public boolean isLeaf() {
+        return left == null && right == null;
+    }
+
+    public boolean hasLeft() {
+        return this.left != null;
+    }
+
+    public boolean hasRight() {
+        return this.right != null;
+    }
+    
+}
---
+++ src/heap/MaxHeap.java
@@ -0,0 +1,152 @@
+import java.util.Arrays;
+import java.util.*;
+
+class MaxHeap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] v = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Heap h = new Heap(v);
+
+        System.out.println(h.isHeap());
+
+        sc.close();
+    }
+}
+
+class Heap {
+    
+    private int[] heap;
+    private int tail;
+    
+    public Heap(int capacidade) {
+        this.heap = new int[capacidade];
+        this.tail = -1;
+    }
+    
+    public Heap(int[] heap) {
+        this.heap = heap;
+        this.tail = this.heap.length - 1;
+    }
+
+    public boolean isEmpty() {
+        return this.tail == -1;
+    }
+
+    public int left(int i) {
+        return 2 * i + 1; 
+    }
+
+    public int right(int i) {
+        return (i + 1) * 2;
+    }
+
+    public int parent(int i) {
+        return (int) (i - 1) / 2;
+    }
+
+    public void add(int n) {
+        if (this.tail >= heap.length - 1) {
+            this.resize();
+        }
+
+        this.heap[++this.tail] = n;
+
+        int index = this.tail;
+        while (index > 0 && heap[index] > heap[parent(index)]) {
+            swap(index, parent(index));
+            index = parent(index);
+        }
+    }
+
+    private void buildHeap() {
+        for (int i = parent(tail); i >= 0; i--) {
+            this.heapify(i);
+        }
+    }
+
+    public boolean isHeap() {
+        for (int i = parent(tail); i >= 0; i--) {
+            if (this.heapify(i) == false) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    public int remove() {
+        if (isEmpty()) throw new RuntimeException();
+
+        int toRemove = this.heap[0];
+        this.heap[0] = this.heap[this.tail--];
+
+        this.heapify(0);
+
+        return toRemove;
+    }
+        
+    private boolean heapify(int index) {
+        if (isLeaf(index) || !isValidIndex(index)) return true;
+
+        int maxIndex = maxIndex(index, left(index), right(index));
+
+        if (maxIndex != index) {
+            return false;
+        }
+
+        return true;
+    } 
+    
+    private int maxIndex(int index, int left, int right) {
+        if (this.heap[index] > this.heap[left]) {
+            if (isValidIndex(right)) {
+                if (this.heap[right] > this.heap[index]) {
+                    return right;
+                }
+            }
+            return index;
+
+        } else {
+            if (isValidIndex(right)) {
+                if (this.heap[right] > this.heap[left]) {
+                    return right;
+                }
+            }
+
+            return left;
+        }
+    }
+
+    private boolean isValidIndex(int index) {
+        return index >= 0 && index <= tail;
+    }
+    
+    private boolean isLeaf(int index) {
+        return index > parent(tail) && index <= tail; 
+    } 
+    
+    private void swap(int i, int j) {
+        int aux = this.heap[i];
+        this.heap[i] = this.heap[j];
+        this.heap[j] = aux;
+    }
+
+    private void resize() {
+        int[] novoHeap = new int[this.heap.length * 2];
+        for (int i = 0; i <= tail; i++)
+            novoHeap[i] = this.heap[i];
+        
+        this.heap = novoHeap;
+    }
+    
+    public int size() {
+        return this.tail + 1;
+    }
+    
+    public String toString() {
+        return Arrays.toString(this.heap);
+    }
+
+}
